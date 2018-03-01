44f4fc3b3386c658519fc7e8687701fb84aed9f4
Melodyn <samelodyn@gmail.com>
2018-03-01 23:22:55 +0000

    Первый фильм

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+require_once 'vendor/autoload.php';
+
+use function App\Renderer\render;
+
+date_default_timezone_set('Europe/Moscow');
+
+$app = new Application();
+
+$app->setRoute('/cinema/', function () {
+    $db = new \App\Databases\Database();
+    return render('index', [
+        'listOfFilms' => $db->select('*', 'films')
+    ]);
+});
+
+$app->run();
