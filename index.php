754173b03c2f458d0b701f0e358ed14419cb041b
melodyn <samelodyn@gmail.com>
2018-03-01 15:49:04 +0000

    Подготовка базы данных

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ require_once 'vendor/autoload.php';
 
 use function App\Renderer\render;
 
+date_default_timezone_set('Europe/Moscow');
+
 $app = new Application();
 
-$app->setRoute('/cinema/', function () {
-    return render('index', ['msg' => "Hello, World!"]);
+$app->setRoute('/cinema/', function () use ($connectDB) {
+    return render('index', [
+        'listOfFilms' => 'list'
+    ]);
 });
 
 $app->run();
---
+++ src/Databases/Database.php
@@ -0,0 +1,26 @@
+<?
+
+namespace Databases;
+
+class Database
+{
+    private $options = [
+		\PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+		\PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+		\PDO::ATTR_EMULATE_PREPARES   => false,
+	];
+    private $host = 'localhost';
+    private $db   = 'db_name';
+    private $user = 'user_name';
+    private $pass = 'password';
+    private $charset = 'utf8';
+    private $pdo;
+
+    public function __construct()
+    {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $this->pdo = new \PDO($dsn, $this->user, $this->pass, $this->options);
+        return $this->pdo;
+    }
+}
+
