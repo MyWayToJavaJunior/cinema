df247e3928ff85f15849ccd561c6aaa8cb47769e
Melodyn <samelodyn@gmail.com>
2018-03-02 16:54:33 +0000

    Форма добавления фильмов

--- public/index.php
+++ public/index.php
@@ -10,18 +10,26 @@ date_default_timezone_set('Europe/Moscow');
 
 $app = new Application();
 
-$app->setRoute('/cinema/', function () {
-    $db = new \App\Databases\Database();
-    return render('index'/*, [
-        'listOfFilms' => $db->select('*', 'films')
-    ]*/);
+$app->get('/cinema/', function () {
+    return render('index');
 });
 
-$app->setRoute('/cinema/admin', function () {
+$app->get('/cinema/admin', function () {
+    return render('admin');
+});
+
+$app->get('/cinema/random', function () {
+    return render('random');
+});
+
+$app->put('/cinema/random', function () {
     $db = new \App\Databases\Database();
-    return render('admin'/*, [
-        'listOfFilms' => $db->select('*', 'seances')
-    ]*/);
+    $insert = $db->insert(
+        'films',
+        ['name', 'length', 'price'],
+        [$_POST['name'], $_POST['length'], $_POST['price']]
+    );
+    return render('random', ['response' => $insert]);
 });
 
 $app->run();
